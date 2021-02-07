eaca4ce0e0f16447d102aa504163a8648db6ecd0
Tommy Anfosso <anfossotommy@hotmail.fr>
2021-02-07 14:14:04 +0100

    extract binaire OK

--- main.cpp
+++ main.cpp
@@ -8,23 +8,23 @@ int main()
     // Fait évoluer une entité ayant pour forme initiale 91 (int)
     // Entite<Forme>{91}.evolve();
 
-    // insert into file
+    /* insert into file */
     {
         Entite<Forme> e{91};
         std::fstream os("test", std::fstream::out | std::fstream::binary | std::fstream::trunc);
         os << e;
     }
 
-    // extract from file (ne marche pas)
+    /* extract from file */
     {
-        Entite<Forme> e{91};
+        Entite<Forme> e{"valeurpardefaut"};
         std::fstream is("test", std::fstream::in | std::fstream::binary);
         is >> e;
         std::visit(overload {
             [](const int& d) { std::cout<<d<<std::endl; },
             [](const float& f) { std::cout<<f<<std::endl; },
             [](const std::string& str) { std::cout<<str<<std::endl; },
-            [](auto& onsaitpas) { std::cout<<"on sait pas"<<std::endl; }
+            [](auto onsaitpas){ std::cout<<"onsaitpas"<<std::endl; }
         }, e.forme);
     }
 }
