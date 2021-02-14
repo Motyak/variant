01c42d14cca1fc6dcc80a3291ec67310dc70de35
Tommy Anfosso <anfossotommy@hotmail.fr>
2021-02-14 19:30:12 +0100

    je comprends pas

--- main.cpp
+++ main.cpp
@@ -2,27 +2,18 @@
 
 #include <fstream>
 #include <vector>
-#include <mutex>
 
 #define FILENAME "test"
 
-void doIt(unsigned& index, std::mutex& mut)
+void doIt()
 {
-    std::ifstream is(FILENAME, std::fstream::binary);
+    std::ifstream is(FILENAME, std::ifstream::binary);
     Entite<Forme> ent{0};
-
-    mut.lock();
-    is.seekg(sizeof(Forme) * index++, std::ios_base::beg);
-    mut.unlock();
 
     while(is.peek() != std::ifstream::traits_type::eof())
     {
         is >> ent;
-        ent.evolve();
-
-        mut.lock();
-        is.seekg(sizeof(Forme) * index++, std::ios_base::beg);
-        mut.unlock();
+        // ent.evolve();
     }
 }
 
@@ -31,7 +22,8 @@ int main()
 {
     /* Enregistrer des entités pour effectuer le test */
     {
-        std::ofstream os(FILENAME, std::fstream::binary | std::fstream::trunc);
+        std::ofstream os(FILENAME, std::ofstream::binary | std::ofstream::trunc);
+
         os << Entite<Forme>{91} << Entite<Forme>{13.37f} << Entite<Forme>{"unechaine"}
            << Entite<Forme>{10} << Entite<Forme>{10.37f} << Entite<Forme>{"chaine10"}
            << Entite<Forme>{20} << Entite<Forme>{20.37f} << Entite<Forme>{"chaine20"}
@@ -40,15 +32,24 @@ int main()
     
     /* Faire évoluer les entités */
     {
-        const unsigned POOL_SIZE = std::thread::hardware_concurrency();
-        // const unsigned POOL_SIZE = 3;
+        // const unsigned POOL_SIZE = std::thread::hardware_concurrency();
+        const unsigned POOL_SIZE = 1;
         std::vector<std::thread> pool;
-        unsigned index = 0;
-        std::mutex mut;
-        for(int i = 0; i < POOL_SIZE; ++i)
-            pool.push_back(std::thread(doIt, std::ref(index), std::ref(mut)));
+        // for(int i = 0; i < POOL_SIZE; ++i)
+        //     pool.push_back(std::thread(doIt));
 
-        for(auto& t : pool)
-            t.join();
+        // for(auto& t : pool)
+        //     t.join();
+
+
+
+        std::ifstream is(FILENAME, std::ifstream::binary);
+        Entite<Forme> ent{0};
+
+        while(is.peek() != std::ifstream::traits_type::eof())
+        {
+            is >> ent;
+            // ent.evolve();
+        }
     }
 }
