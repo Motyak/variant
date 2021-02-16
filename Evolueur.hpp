bb32254451a7199e588c4277eb9bc2683fa962fd
Tommy Anfosso <anfossotommy@hotmail.fr>
2021-02-16 18:57:06 +0100

    ecriture OK

--- Evolueur.hpp
+++ Evolueur.hpp
@@ -2,51 +2,39 @@
 #define EVOLUEUR_H
 
 #include <iostream>
-
-/* DEBUG *////////////////////////////////////////////////////////////////////
 #include <variant>
+
 template<class... Ts> struct overload : Ts... { using Ts::operator()...; };
 template<class... Ts> overload(Ts...) -> overload<Ts...>;
 
-using Forme = std::variant<int,float,std::string>;
-/* DEBUG *///////////////////////////////////////////////////////////////////
-
-// debug
-void afficher(const Forme& forme)
-{
-    std::visit(overload {
-        [](const int& d) { std::cout<<d<<std::endl; },
-        [](const float& f) { std::cout<<f<<std::endl; },
-        [](const std::string& str) { std::cout<<str<<std::endl; },
-        [](auto onsaitpas){ std::cout<<"onsaitpas"<<std::endl; }
-    }, forme);
-}
-
 template<typename... Types>
-struct Evoluable : public std::variant<Types>
+struct Evoluable
 {
-    // T forme;
+    std::variant<Types...> forme;
 
-    // Evoluable(T formeInitiale) : forme(formeInitiale){}
+    Evoluable(std::variant<Types...> formeInitiale) : forme(formeInitiale){}
 
-    virtual void evolve() = 0;
+//     virtual void evolve() = 0;
 
     friend std::ostream& operator<<(std::ostream& out, const Evoluable& e) 
     {
         return std::visit(overload {
-            [](const std::string& str) -> std::ofstream& { return out << str; },
-            [](auto else) -> std::ofstream& {
+            [&](const std::string& str) -> std::ostream& {
+                std::size_t s = sizeof(char) * str.length();
+                out.write((char*)&s, sizeof(std::size_t));
+                return out << str;
+            },
+            [&](auto other) -> std::ostream& {
                 e.insert(out);
                 return out;
             }
         }, e.forme);
-        
     }
-    friend std::istream& operator>>(std::istream& in, Evoluable& e)
-    {
-        e.extract(in);
-        return in;
-    }
+    // friend std::istream& operator>>(std::istream& in, Evoluable& e)
+    // {
+    //     e.extract(in);
+    //     return in;
+    // }
 
   private:
     virtual void insert(std::ostream& out) const
@@ -54,18 +42,24 @@ struct Evoluable : public std::variant<Types>
         std::size_t s = sizeof(this->forme);
         std::cout<<"size of next elem : "<<s<<std::endl;
         out.write((char*)&s, sizeof(std::size_t));
-        afficher(this->forme);
+
+        // affichage debug
+        std::visit(overload {
+            [](const int& d) { std::cout<<d<<std::endl; },
+            [](const float& f) { std::cout<<f<<std::endl; },
+            [](const std::string& str) { std::cout<<str<<std::endl; },
+            [](auto onsaitpas){ std::cout<<"onsaitpas"<<std::endl; }
+        }, this->forme);
+
         out.write((char*)&this->forme, s);
     }
-    virtual void extract(std::istream& in)
-    {
-        std::size_t s;
-        in.read((char*)&s, sizeof(std::size_t));
-        std::cout<<"size of next elem : "<<s<<std::endl;
-        in.read((char*)&this->forme, s);
-    }
+//     virtual void extract(std::istream& in)
+//     {
+//         std::size_t s;
+//         in.read((char*)&s, sizeof(std::size_t));
+//         std::cout<<"size of next elem : "<<s<<std::endl;
+//         in.read((char*)&this->forme, s);
+//     }
 };
-
-class Evolueur;
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,85 +1,33 @@
-#include "Entite.hpp"
+#include "Evolueur.hpp"
 
 #include <fstream>
-#include <vector>
-#include <mutex>
 
 #define FILENAME "test"
-
-void doIt(std::istream& is, std::mutex& mut)
-{
-    Entite<Forme> ent{0};
-
-    while(true)
-    {
-        /* extraction du prochain élément */
-        mut.lock();
-        if(is.peek() == std::ifstream::traits_type::eof())
-        {
-            mut.unlock();
-            return;
-        }
-        is >> ent;
-        mut.unlock();
-
-        /* operation sur l'element */
-        // ent.evolve();
-    }
-}
 
 // g++ main.cpp -std=c++17 -pthread
 int main()
 {
-    /* Enregistrer des entités pour effectuer le test */
+    using Ev = Evoluable<int,float,std::string>;
+
+    /* Ecriture */
     {
         std::ofstream os(FILENAME, std::ofstream::binary | std::ofstream::trunc);
+        os << Ev{91} << Ev{13.37f} << Ev{"unechaine"};
+    }
 
-        os << Entite<Forme>{91};
-        os << Entite<Forme>{"123456789123456789123456789123456789"};
-        os << Entite<Forme>{13.37f};
+    /* Lecture */
+    {
+        // std::ifstream is(FILENAME, std::ifstream::binary);
+        // Evoluable<int,float,std::string> ev{0};
+        // is >> ent;
 
-        // os << Entite<Forme>{91} << Entite<Forme>{13.37f} << Entite<Forme>{"unechaine"}
-        //    << Entite<Forme>{10} << Entite<Forme>{10.37f} << Entite<Forme>{"chaine10"}
-        //    << Entite<Forme>{20} << Entite<Forme>{20.37f} << Entite<Forme>{"chaine20"}
-        //    << Entite<Forme>{30} << Entite<Forme>{30.37f} << Entite<Forme>{"chaine30"};
+        // // affichage debug
+        // std::visit(overload {
+        //     [](const int& d) { std::cout<<d<<std::endl; },
+        //     [](const float& f) { std::cout<<f<<std::endl; },
+        //     [](const std::string& str) { std::cout<<str<<std::endl; },
+        //     [](auto onsaitpas){ std::cout<<"onsaitpas"<<std::endl; }
+        // }, ev);
     }
     
-    /* LECTURE DU FICHIER SEQUENTIELLE */
-    {
-        std::ifstream is(FILENAME, std::ifstream::binary);
-        Entite<Forme> ent{0};
-        is >> ent; afficher(ent.forme);
-        is >> ent; afficher(ent.forme);
-        is >> ent; afficher(ent.forme);
-        // is >> ent; afficher(ent.forme);
-        // is >> ent; afficher(ent.forme);
-        // is >> ent; afficher(ent.forme);
-        // is >> ent; afficher(ent.forme);
-        // is >> ent; afficher(ent.forme);
-        // is >> ent; afficher(ent.forme);
-        // is >> ent; afficher(ent.forme);
-        // is >> ent; afficher(ent.forme);
-        // is >> ent; afficher(ent.forme);////////////////////////////
-
-
-        // while(is.peek() != std::ifstream::traits_type::eof())
-        // {
-        //     is >> ent;
-        //     // ent.evolve();
-        // }
-    }
-
-    /* Faire évoluer les entités */
-    // {
-    //     // const unsigned POOL_SIZE = std::thread::hardware_concurrency();
-    //     const unsigned POOL_SIZE = 1;
-    //     std::ifstream is(FILENAME, std::ifstream::binary);
-    //     std::mutex mut;
-    //     std::vector<std::thread> pool;
-    //     for(int i = 0; i < POOL_SIZE; ++i)
-    //         pool.push_back(std::thread(doIt, std::ref(is), std::ref(mut)));
-
-    //     for(auto& t : pool)
-    //         t.join();
-    // }
 }
