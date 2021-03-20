68bef8ae0328765d8c84ced4d485ca091b9f7905
Tommy Anfosso <anfossotommy@hotmail.fr>
2021-03-20 04:39:48 +0100

    debut docker

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM alpine:3.13
+
+ADD src src
+
+RUN apk add --no-cache g++ && \
+    apk add --no-cache make && \
+    apk add --no-cache cmake && \
+    mkdir -p usr/local/include && \
+    cp -r src/cereal/include/cereal usr/local/include/cereal && \
+    cd src/hiredis && \
+    make && \
+    make install && \
+    cd ../redis-plus-plus && \
+    mkdir build && \
+    cd build && \
+    cmake -DREDIS_PLUS_PLUS_CXX_STANDARD=17 -DREDIS_PLUS_PLUS_BUILD_TEST=OFF -DREDIS_PLUS_PLUS_BUILD_STATIC=OFF .. && \
+    make && \
+    make install && \
+    export LD_LIBRARY_PATH=/usr/local/lib && \
+    ldconfig && \
+    g++ -std=c++17 /src/main.cpp -o /evolueur -pthread -lredis++ -lhiredis && \
+    cd / && \
+    rm -rf src && \
+    apk del g++ make cmake
+
+ENTRYPOINT ["./evolueur"]
---
+++ src/Evoluable.hpp
@@ -0,0 +1,45 @@
+#ifndef EVOLUABLE_H
+#define EVOLUABLE_H
+
+#include <cereal/types/polymorphic.hpp>
+#include <cereal/archives/binary.hpp>
+#include <cereal/types/variant.hpp>
+
+template<class... Ts> struct Evolution : Ts... { using Ts::operator()...; };
+template<class... Ts> Evolution(Ts...) -> Evolution<Ts...>;
+
+struct Evoluable
+{
+    virtual bool evoluer() = 0;
+
+    template<typename... Types>
+    struct Forme
+    {
+        std::variant<Types...> forme;
+
+        Forme() = default;
+        Forme(const std::variant<Types...>& formeInitiale) : forme(formeInitiale){}
+
+        template<class Archive>
+        void serialize(Archive& ar) { ar(forme); }
+    };
+};
+CEREAL_REGISTER_TYPE(Evoluable);
+
+using EvoluablePtr = std::shared_ptr<Evoluable>;
+
+std::ostream& operator<<(std::ostream& os, const EvoluablePtr& ev)
+{
+    cereal::BinaryOutputArchive oarchive(os);
+    oarchive(ev);
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, EvoluablePtr& ev)
+{
+    cereal::BinaryInputArchive iarchive(is);
+    iarchive(ev);
+    return is;
+}
+
+#endif
